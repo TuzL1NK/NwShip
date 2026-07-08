c404a3de49010e05bcea6a4ca5d027bc3859fce6
TuzL1NK <zhangq15@outlook.com>
2026-07-08 23:16:28 +0800

    chore: remove tracked files that are now in .gitignore

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/NwShip.dir/NwShip_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/NwShip.dir/NwShip_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- D:\ProgramData\QtProject\NwShip\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\NwShip_autogen\EWIEGA46WW\qrc_resources.cpp
